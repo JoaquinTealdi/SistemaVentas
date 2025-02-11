2e15341b9d582082b082975d4eb460b761e9c59d
Joaquin Tealdi <joaquintealdi25@gmail.com>
2025-02-11 19:48:29 -0300

    add singleton to save logued user

---
+++ SaleSystem/SS.Services/UserSession.cs
@@ -0,0 +1,29 @@
+﻿using SS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SS.Services
+{
+    public sealed class UserSession
+    {
+        private static readonly Lazy<UserSession> _instance = new Lazy<UserSession>(() => new UserSession());
+        public static UserSession Instance => _instance.Value;
+
+        public User CurrentUser { get; private set; }
+
+        private UserSession() { }
+
+        public void Login(User user)
+        {
+            CurrentUser = user;
+        }
+
+        public void Logout()
+        {
+            CurrentUser = null;
+        }
+    }
+}
